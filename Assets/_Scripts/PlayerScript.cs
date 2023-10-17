d4b9a46bc33fdab1fb6da1a69d329db56ce816b8
Nicholas-Chu <nick.chu75@gmail.com>
2023-10-17 16:39:04 -0700

    fixed all raycasting, physics, line render

--- Assets/_Scripts/PlayerScript.cs
+++ Assets/_Scripts/PlayerScript.cs
@@ -7,7 +7,7 @@ public class PlayerScript : MonoBehaviour
 {
     public float power = 5f;
     public float maxDragDistance = 2f;
-    public Vector3 minScale = new Vector3(0f, 0f, 0f);
+    public Vector3 minScale = new Vector3(0.05f, 0.05f, 0.05f);
     public Vector3 maxScale = new Vector3(0.3f, 0.3f, 0.3f); // Assuming initial scale is (1, 1, 1)
     public LineRenderer powerIndicator;
 
@@ -16,6 +16,7 @@ public class PlayerScript : MonoBehaviour
     //private Vector3 endPoint;
     private Vector3 direction;
     private bool isDragging = false;
+    public float lineRendererZOffset = 0.01f;
 
     private void Start()
     {
@@ -25,34 +26,42 @@ public class PlayerScript : MonoBehaviour
 
     private void Update()
     {
-        if (Input.GetMouseButtonDown(0))
+        if (transform.localScale == minScale)
         {
-            isDragging = true;
-            OnDragStart();
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+            return;  // Exit out of Update for this frame
         }
-        if (Input.GetMouseButtonUp(0))
+
+        // Only allow shooting if the ball is almost stationary
+        if (rb.velocity.magnitude < 0.1f)
         {
-            isDragging = false;
-            OnDragEnd();
+            if (Input.GetMouseButtonDown(0))
+            {
+                isDragging = true;
+                OnDragStart();
+            }
+            if (Input.GetMouseButtonUp(0))
+            {
+                isDragging = false;
+                OnDragEnd();
+            }
         }
 
         if (isDragging)
         {
             OnDrag();
         }
-        /*if (rb.velocity.magnitude > 0.1f)
+
+        if (rb.velocity.magnitude > 0.2f)
         {
-            transform.localScale = Vector3.Lerp(transform.localScale, minScale, 0.01f);
-        }*/
-        if (rb.velocity.magnitude > 0.1f)
-        {
-            transform.localScale = Vector3.MoveTowards(transform.localScale, minScale, 0.0001f);
+            transform.localScale = Vector3.MoveTowards(transform.localScale, minScale, 0.00007f);
         }
     }
 
     void OnDragStart()
     {
         startPoint = GetMouseWorldPosition();
+        Vector3 adjustedStartPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + lineRendererZOffset);
         powerIndicator.SetPosition(0, transform.position);
         powerIndicator.enabled = true;
     }
@@ -66,6 +75,7 @@ public class PlayerScript : MonoBehaviour
         Vector3 currentPoint = GetMouseWorldPosition();
         direction = currentPoint - startPoint; // Invert this for opposite drag direction
         float distance = Mathf.Clamp(Vector3.Distance(startPoint, currentPoint), 0, maxDragDistance);
+        Vector3 adjustedEndPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + lineRendererZOffset) - direction.normalized * distance;
         powerIndicator.SetPosition(1, transform.position - direction.normalized * distance);
     }
 
@@ -88,9 +98,18 @@ public class PlayerScript : MonoBehaviour
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         RaycastHit hit;
 
-        if (Physics.Raycast(ray, out hit))
+        int layerMask = ~(LayerMask.GetMask("Trees", "Chalet", "Fence", "Border"));
+
+        if (Physics.Raycast(ray, out hit, Mathf.Infinity, layerMask))
         {
             return hit.point;
+        }
+
+        Plane playerDepthPlane = new Plane(Vector3.up, transform.position); // This assumes Z is your depth axis. Adjust if necessary.
+        float distanceToPlane;
+        if (playerDepthPlane.Raycast(ray, out distanceToPlane))
+        {
+            return ray.GetPoint(distanceToPlane);
         }
 
         return Vector3.zero;
