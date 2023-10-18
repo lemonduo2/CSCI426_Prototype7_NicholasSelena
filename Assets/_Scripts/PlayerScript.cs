586896ad3c9835e3de5b8ed2b7fae2797a327535
lemonduo2 <yiyunxu@usc.edu>
2023-10-18 04:43:46 -0700

    indicators update and vfx

--- Assets/_Scripts/PlayerScript.cs
+++ Assets/_Scripts/PlayerScript.cs
@@ -1,15 +1,20 @@
+using MoreMountains.Feedbacks;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PlayerScript : MonoBehaviour
 {
     public float power = 5f;
-    public float maxDragDistance = 2f;
+    public float maxDragDistance = 4f;
     public Vector3 minScale = new Vector3(0.05f, 0.05f, 0.05f);
     public Vector3 maxScale = new Vector3(0.3f, 0.3f, 0.3f); // Assuming initial scale is (1, 1, 1)
     public LineRenderer powerIndicator;
+
+    public GameObject indicatorCanvas;
+    public GameObject selfIndicator;
 
     private Rigidbody rb;
     private Vector3 startPoint;
@@ -18,14 +23,19 @@ public class PlayerScript : MonoBehaviour
     private bool isDragging = false;
     public float lineRendererZOffset = 0.01f;
 
+    public MMFeedbacks moveFeedback;
+
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
         powerIndicator.enabled = false;
+        selfIndicator.SetActive(true);
     }
 
     private void Update()
     {
+        indicatorCanvas.transform.position = new Vector3 (transform.position.x, indicatorCanvas.transform.position.y, transform.position.z);
+
         if (transform.localScale == minScale)
         {
             RestartCurrentScene();
@@ -72,14 +82,35 @@ public class PlayerScript : MonoBehaviour
             OnDrag();
         }
 
-        if (rb.velocity.magnitude > 0.2f)
+        if (rb.velocity.magnitude >= 0.6f)
         {
+            moveFeedback.PlayFeedbacks(transform.position);
+        }
+        
+        if (rb.velocity.magnitude >= 0.2f)
+        {
+            selfIndicator.SetActive(false);
             transform.localScale = Vector3.MoveTowards(transform.localScale, minScale, 0.00007f);
+        }
+        else
+        {
+            selfIndicator.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Application.Quit();
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            RestartCurrentScene();
         }
     }
 
     void OnDragStart()
     {
+        selfIndicator.SetActive(true);
         if (rb.velocity.magnitude >= 0.1f)
         {
             isDragging = false;
@@ -90,20 +121,23 @@ public class PlayerScript : MonoBehaviour
         powerIndicator.SetPosition(0, transform.position);
         powerIndicator.SetPosition(1, transform.position);
         powerIndicator.enabled = true;
+        
     }
 
     void OnDrag()
     {
+        selfIndicator.SetActive(true);
         Vector3 currentPoint = GetMouseWorldPosition();
         direction = currentPoint - startPoint; // Invert this for opposite drag direction
         float distance = Mathf.Clamp(Vector3.Distance(startPoint, currentPoint), 0, maxDragDistance);
+
         Vector3 adjustedEndPosition = new Vector3(transform.position.x, transform.position.y, transform.position.z + lineRendererZOffset) - direction.normalized * distance;
         powerIndicator.SetPosition(1, transform.position - direction.normalized * distance);
     }
 
     void OnDragEnd()
     {
-        if (rb.velocity.magnitude >= 0.1f)
+        if (rb.velocity.magnitude >= 0.2f)
         {
             powerIndicator.enabled = false; // Disable the line renderer if the object is moving
             return;
@@ -113,6 +147,7 @@ public class PlayerScript : MonoBehaviour
         float distance = Vector3.Distance(powerIndicator.GetPosition(0), powerIndicator.GetPosition(1));
         rb.AddForce(-direction * distance * power, ForceMode.Impulse); // Note the negative sign to invert the direction
         powerIndicator.enabled = false;
+        selfIndicator.SetActive(true);
         isDragging = false;
     }
 
